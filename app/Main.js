c5a3e6dd6e7f1ee96ec494ba30a71144591c20e3
mr-blankson <fat_tonyy@icloud.com>
2020-06-07 16:38:41 +0000

    fixed-issues-by-switching-to-React-Router-dom :octocat:

--- app/Main.js
+++ app/Main.js
@@ -2,15 +2,31 @@ import React from "react";
 import ReactDom from "react-dom";
 import Header from "./components/Header";
 import HomeGuest from "./components/HomeGuest";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 import Footer from "./components/Footer";
+import About from "./components/About";
+import Terms from "./components/Terms";
 
 function Main() {
 	return (
-		<>
+		<BrowserRouter>
 			<Header />
-			<HomeGuest />
+			<Switch>
+				<Route path="/" exact>
+					{" "}
+					<HomeGuest />
+				</Route>
+				<Route path="/about-us" exact>
+					{" "}
+					<About />
+				</Route>
+				<Route path="/terms" exact>
+					{" "}
+					<Terms />
+				</Route>
+			</Switch>
 			<Footer />
-		</>
+		</BrowserRouter>
 	);
 }
 
